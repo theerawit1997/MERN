a522c8d2f229a9f3305b9f9c24912c18706a4e0c
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2023-01-26 04:58:33 +0700

    modified:   client/src/App.js 	modified:   client/src/App.js 	modified:   client/src/MyRoute.js 	modified:   server/controllers/blogController.js 	modified:   server/routes/blog.js 	client/src/components/EditComponent.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import NavbarComponent from "./components/NavbarComponent";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function App() {
   const [blogs, setBlogs] = useState([])
@@ -16,7 +17,24 @@ function App() {
   }
   useEffect(() => {
     fetchData()
-  })
+  }, [])
+
+  const confirmDelete = (slug) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Deleted!', 'Your file has been deleted.', 'success')
+      }
+    })
+  }
+
   return (
     <div className="container p-5">
       <NavbarComponent></NavbarComponent>
@@ -28,6 +46,8 @@ function App() {
             </Link>
             <p>{blog.content.substring(0, 300)}...</p>
             <p className="text-muted">Author: {blog.author}, created at: {new Date(blog.createdAt).toLocaleString()}</p>
+            <button className="btn btn-outline-success">update</button> &nbsp;
+            <button className="btn btn-outline-danger" onClick={() => confirmDelete(blog.slug)}>remove</button>
           </div>
         </div>
       ))}
