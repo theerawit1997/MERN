020296126dc5ffe77ded42fd70992b21b39de5ce
theerawit1997 <79015789+theerawit1997@users.noreply.github.com>
2023-01-31 14:20:22 +0700

    create login token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,12 @@
+const jwt = require("jsonwebtoken")
+// const expressJWT = require("express-jwt")
+
 exports.login = (req, res) => {
-    res.json({
-        date: "login test"
-    })
+    const { username, password } = req.body
+    if (password === process.env.PASSWORD) {
+        const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1d' })
+        return res.json({ token, username })
+    } else {
+        return res.status(400).json({ error: "password is incorrect" })
+    }
 }
